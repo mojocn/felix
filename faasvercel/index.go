710aadcda60b8bc36ec1569b58fd6fe25a208b1c
mojocn <neochau@gmail.com>
2024-11-14 10:36:11 +0800

    faas-vercel

---
+++ faasvercel/index.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Demo(w http.ResponseWriter, r *http.Request) {
+	// hello world
+
+	// response json hello world
+	w.Write([]byte("hello world1"))
+
+}
+
+func MyInfo(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["ip"] = r.RemoteAddr
+	resp["user-agent"] = r.UserAgent()
+	resp["accept-language"] = r.Header.Get("Accept-Language")
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(jsonResp)
+	}
+}
