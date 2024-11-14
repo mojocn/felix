a2f2f5efe5e3b87145b86953478155f0dd508229
mojocn <neochau@gmail.com>
2024-11-14 11:07:07 +0800

    faas-vercel:SSE example

--- faasvercel/api/sse.go
+++ faasvercel/api/sse.go
@@ -13,15 +13,19 @@ func Sse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Connection", "keep-alive")
 
 	// Create a ticker to send events periodically
-	ticker := time.NewTicker(2 * time.Second)
+	ticker := time.NewTicker(time.Second / 10)
 	defer ticker.Stop()
-
+	cnt := 0
 	for {
 		select {
 		case <-r.Context().Done():
 			// Client closed the connection
 			return
 		case t := <-ticker.C:
+			cnt++
+			if cnt > 20 {
+				return
+			}
 			// Send an event to the client
 			fmt.Fprintf(w, "data: %s\n\n", t.String())
 			// Flush the response to ensure the event is sent
